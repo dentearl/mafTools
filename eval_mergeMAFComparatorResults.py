a0b5f324323a9ed6627ec9b0c44ea4ce49778747
Bernard Suh <bsuh@soe.ucsc.edu>
2010-06-04 02:29:35 +0000

    Updated file with new cmd-line option to merge all the xml files in a given dir

--- eval_mergeMAFComparatorResults.py
+++ eval_mergeMAFComparatorResults.py
@@ -5,6 +5,7 @@
 
 import xml.etree.ElementTree as ET
 import os
+import sys
 
 from sonLib.bioio import getBasicOptionParser
 from sonLib.bioio import parseBasicOptions
@@ -22,6 +23,9 @@ def main():
     
     parser.add_option("--results2", dest="results2", 
                       help="File containing the second XML formatted MAF comparsion results.")
+
+    parser.add_option("--inputDir", dest="inputDir",
+                      help="Directory containing the XML formatted MAF comparison results.")
     
     parser.add_option("--outputFile", dest="outputFile", type="string",
                       help="The file to put the aggregated results in (can be the same as either of the two inputs)")
@@ -31,65 +35,79 @@ def main():
     assert len(args) == 0
     logger.info("Parsed arguments")
     
-    assert options.results1 != None
-    assert options.results2 != None
+    assert (options.results1 != None and options.results2 != None) or (options.inputDir != None)
     assert options.outputFile != None
     
     ##########################################
     #Do the merging.
     ##########################################
-    
-    resultsTree1 = ET.parse(options.results1).getroot()
-    resultsTree2 = ET.parse(options.results2).getroot()
-    
+
+    xmlList = []
+    if options.inputDir != None:
+        try:
+            fileList = os.listdir(options.inputDir)
+        except:
+            print >> sys.stderr, "Error: Can't open dir '%s'" % options.inputDir
+            sys.exit(-1)
+
+        for file in fileList:
+            if file.endswith(".xml"):
+                xmlList.append(options.inputDir + "/" + file)
+    else:
+        xmlList.append(options.results1)
+        xmlList.append(options.results2)
+
+    baseFile = xmlList.pop()
+    resultsTree1 = ET.parse(baseFile).getroot()
     homologyTestsList1 = resultsTree1.findall("homology_tests")
-    homologyTestsList2 = resultsTree2.findall("homology_tests")
+
+    for xmlFile in xmlList:
+        resultsTree2 = ET.parse(xmlFile).getroot()
+        homologyTestsList2 = resultsTree2.findall("homology_tests")
     
-    assert len(homologyTestsList1) == len(homologyTestsList2)
-    for i in xrange(len(homologyTestsList1)):
-        homologyTests1 = homologyTestsList1[i]
-        homologyTests2 = homologyTestsList2[i]
-        #Do merge for tests in both sets
-        for homologyTest1 in homologyTests1.findall("homology_test"):
-            for homologyTest2 in homologyTests2.findall("homology_test"):
-                if homologyTest1.attrib["sequenceA"] == homologyTest2.attrib["sequenceA"] and \
-                homologyTest1.attrib["sequenceB"] == homologyTest2.attrib["sequenceB"]:
-                    
-                    homologyTest1.attrib["totalTests"] = str(int(homologyTest1.attrib["totalTests"]) + int(homologyTest2.attrib["totalTests"]))
-                    homologyTest1.attrib["totalTrue"] = str(int(homologyTest1.attrib["totalTrue"]) + int(homologyTest2.attrib["totalTrue"]))
-                    homologyTest1.attrib["totalFalse"] = str(int(homologyTest1.attrib["totalFalse"]) + int(homologyTest2.attrib["totalFalse"]))
-                    if int(homologyTest1.attrib["totalTests"]) != 0:
-                        homologyTest1.attrib["average"] = str(float(homologyTest1.attrib["totalTrue"]) /  float(homologyTest1.attrib["totalTests"]))
-                    
-                    for singleHomologyTest2 in homologyTest2.findall("single_homology_test"):
-                        homologyTest1.insert(0, singleHomologyTest2)
-        
-        #Now add in tests not in the intersection of the results
-        l = []
-        for homologyTest2 in homologyTests2.findall("homology_test"):
+        assert len(homologyTestsList1) == len(homologyTestsList2)
+        for i in xrange(len(homologyTestsList1)):
+            homologyTests1 = homologyTestsList1[i]
+            homologyTests2 = homologyTestsList2[i]
+            #Do merge for tests in both sets
             for homologyTest1 in homologyTests1.findall("homology_test"):
-                if homologyTest1.attrib["sequenceA"] == homologyTest2.attrib["sequenceA"] and \
-                    homologyTest1.attrib["sequenceB"] == homologyTest2.attrib["sequenceB"]:
-                    break
-            else:
-                l.append(homologyTest2)
+                for homologyTest2 in homologyTests2.findall("homology_test"):
+                    if homologyTest1.attrib["sequenceA"] == homologyTest2.attrib["sequenceA"] and homologyTest1.attrib["sequenceB"] == homologyTest2.attrib["sequenceB"]:
+                    
+                        homologyTest1.attrib["totalTests"] = str(int(homologyTest1.attrib["totalTests"]) + int(homologyTest2.attrib["totalTests"]))
+                        homologyTest1.attrib["totalTrue"] = str(int(homologyTest1.attrib["totalTrue"]) + int(homologyTest2.attrib["totalTrue"]))
+                        homologyTest1.attrib["totalFalse"] = str(int(homologyTest1.attrib["totalFalse"]) + int(homologyTest2.attrib["totalFalse"]))
+                        if int(homologyTest1.attrib["totalTests"]) != 0:
+                            homologyTest1.attrib["average"] = str(float(homologyTest1.attrib["totalTrue"]) /  float(homologyTest1.attrib["totalTests"]))
+                    
+                        for singleHomologyTest2 in homologyTest2.findall("single_homology_test"):
+                            homologyTest1.insert(0, singleHomologyTest2)
         
-        for homologyTest2 in l:
-            homologyTests1.insert(0, homologyTest2)
+            #Now add in tests not in the intersection of the results
+            l = []
+            for homologyTest2 in homologyTests2.findall("homology_test"):
+                for homologyTest1 in homologyTests1.findall("homology_test"):
+                    if homologyTest1.attrib["sequenceA"] == homologyTest2.attrib["sequenceA"] and homologyTest1.attrib["sequenceB"] == homologyTest2.attrib["sequenceB"]:
+                        break
+                else:
+                    l.append(homologyTest2)
+        
+            for homologyTest2 in l:
+                homologyTests1.insert(0, homologyTest2)
             
-        #Now recalculate the totals
-        totalTrue = 0
-        totalFalse = 0
-        totalTests = 0
-        for homologyTest1 in homologyTests1.findall("homology_test"):
-            totalTests += int(homologyTest1.attrib["totalTests"])
-            totalTrue += int(homologyTest1.attrib["totalTrue"])
-            totalFalse += int(homologyTest1.attrib["totalFalse"])
-        assert totalTrue + totalFalse == totalTests
-        homologyTests1.attrib["totalTests"] = str(totalTests)
-        homologyTests1.attrib["totalTrue"] = str(totalTrue)
-        homologyTests1.attrib["totalFalse"] = str(totalFalse)
-        homologyTests1.attrib["average"] = str(float(totalTrue)/(totalTests + 0.01))
+            #Now recalculate the totals
+            totalTrue = 0
+            totalFalse = 0
+            totalTests = 0
+            for homologyTest1 in homologyTests1.findall("homology_test"):
+                totalTests += int(homologyTest1.attrib["totalTests"])
+                totalTrue += int(homologyTest1.attrib["totalTrue"])
+                totalFalse += int(homologyTest1.attrib["totalFalse"])
+            assert totalTrue + totalFalse == totalTests
+            homologyTests1.attrib["totalTests"] = str(totalTests)
+            homologyTests1.attrib["totalTrue"] = str(totalTrue)
+            homologyTests1.attrib["totalFalse"] = str(totalFalse)
+            homologyTests1.attrib["average"] = str(float(totalTrue)/(totalTests + 0.01))
         
     
     #Write to the results file.
@@ -97,8 +115,9 @@ def main():
     tree = ET.ElementTree(resultsTree1)
     tree.write(fileHandle)
     fileHandle.close()
-    
 
+    return
+    
 def _test():
     import doctest      
     return doctest.testmod()
