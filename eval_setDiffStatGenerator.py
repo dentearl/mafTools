d0e8eefdf226a22a1f1c676f43473d51e35fe916
Dent Earl <dearl@soe.ucsc.edu>
2010-11-08 23:03:50 +0000

    initial checkin of the setDiffStatGenerator. Useful for parsing the output of setDiffDroppedMissing, and then creating test case mafs for Mark.

---
+++ eval_setDiffStatGenerator.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+"""
+
+"""
+##############################
+import os
+import re
+import sys
+from optparse import OptionParser
+
+def initOptions(parser):
+    parser.add_option('-s', '--second', dest='second', action='store_true',
+                      default=False, help='Insead of taking first sequence in the pair, take the second.')
+
+def checkOptions(options):
+    pass
+
+def readStream(options):
+    sizeDict = {}
+    positionDict = {}
+    nameDict = {}
+    offset = 0
+    if options.second:
+        offset = 2
+    startPos = -1
+    currLength  = 0
+    previousPos = -1
+    for line in sys.stdin:
+        t = line.split('\t')
+        if t[ 0 ][0] == '#':
+            continue
+        if int( t[ 2 + offset ] ) == previousPos:
+            continue
+        if int( t[ 2 + offset ] ) == previousPos + 1:
+            currLength += 1
+            previousPos = int( t[ 2 + offset ] )
+            continue
+        if currLength in sizeDict:
+            sizeDict[ currLength ] += 1
+            positionDict[ currLength ] = startPos
+            nameDict[ currLength ] = t[ 1 + offset ]
+        else:
+            sizeDict[ currLength ]  = 1
+            positionDict[ currLength ] = startPos
+            nameDict[ currLength ] = t[ 1 + offset ]
+        currLength = 1
+        startPos = int( t[ 2 + offset ] )
+        previousPos = int( t[ 2 + offset ] )
+    if currLength in sizeDict:
+        sizeDict[ currLength ] += 1
+    else:
+        sizeDict[ currLength ]  = 1
+    keys = sizeDict.keys()
+    keys.sort()
+    print ' bp\tCount\tMstR pos\tMstR name'
+    for k in keys:
+        print '%3d\t%d\t%d\t%s' % ( k, sizeDict[ k ], positionDict[ k ], nameDict[ k ] )
+
+def main():
+    parser=OptionParser()
+    initOptions(parser)
+    (options, args) = parser.parse_args()
+    checkOptions(options)
+
+    readStream( options )
+
+
+if __name__ == "__main__":
+    main()
