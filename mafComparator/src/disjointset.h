b72b8883dbe3e253165f761487947026f74bd195
Dent Earl <dearl@soe.ucsc.edu>
2011-02-21 15:21:47 -0800

    added license information to all source files.

--- mafComparator/src/disjointset.h
+++ mafComparator/src/disjointset.h
@@ -1,9 +1,35 @@
-/*
- * Disjoint-Set Data Structure
- *
+/* Disjoint-Set Data Structure
+ * 
  * Implementation of the disjoint-set forest data structure.
  * Based off pseudocode in "Introduction to Algorithms" by Cormen, et al.
- */
+ * 
+ * Copyright (C) 2009-2011 by 
+ * Bernard Suh (bsuh@soe.ucsc.edu)
+ * Dent Earl (dearl@soe.ucsc.edu, dentearl@gmail.com)
+ * Benedict Paten (benedict@soe.ucsc.edu, benedictpaten@gmail.com)
+ * Mark Diekhans (markd@soe.ucsc.edu)
+ * ... and other members of the Reconstruction Team of David Haussler's 
+ * lab (BME Dept. UCSC).
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE. 
+*/
+
 
 #ifndef DISJOINTSET_H_
 #define DISJOINTSET_H_
--- mafComparator/src/getRepeatBedTest.py
+++ mafComparator/src/getRepeatBedTest.py
@@ -1,6 +1,34 @@
 import unittest
 import os
 
+"""
+Copyright (C) 2009-2011 by 
+Dent Earl (dearl@soe.ucsc.edu, dentearl@gmail.com)
+Benedict Paten (benedict@soe.ucsc.edu, benedictpaten@gmail.com)
+Mark Diekhans (markd@soe.ucsc.edu)
+... and other members of the Reconstruction Team of David Haussler's 
+lab (BME Dept. UCSC).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+"""
+
 from sonLib.bioio import system
 from sonLib.bioio import getTempFile
 
