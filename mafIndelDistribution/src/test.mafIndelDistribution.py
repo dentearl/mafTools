fdf31a6981f36031a137d966727a0436314e9529
Dent Earl <dearl@soe.ucsc.edu>
2011-12-16 10:44:19 -0800

    added --noEdges mode that turns off recording gaps that occur at the edges of chromosomes.

--- mafIndelDistribution/src/test.mafIndelDistribution.py
+++ mafIndelDistribution/src/test.mafIndelDistribution.py
@@ -281,6 +281,7 @@ class VerifyMafRead(unittest.TestCase):
         shutil.rmtree(os.path.dirname(self.options.maf))
 
 class VerifyAnalysis(unittest.TestCase):
+    # gaps defined to exist anywhere, even at the edges of alignments
     knownValues = [('case 1',
                     [numpy.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], dtype = numpy.uint16),
                      numpy.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], dtype = numpy.uint16)], 
@@ -304,6 +305,33 @@ class VerifyAnalysis(unittest.TestCase):
                     [4, 3, 3, 2, 1, 1, 1, 1, 2], [3, 5, 4], ),
                    ]
     options = GenericObject()
+    options.noEdges = False
+
+    # gaps defined to exist only between regions of alignment
+    knownValuesNoEdges = [('case 1',
+                           [numpy.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], dtype = numpy.uint16),
+                            numpy.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], dtype = numpy.uint16)], 
+                           [], [10, 10] ),
+                          ('case 2',
+                           [numpy.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype = numpy.uint16),
+                            numpy.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype = numpy.uint16)],
+                           [], [0, 0], ),
+                          ('case 3',
+                           [numpy.array([0, 0, 0, 0, 1, 1, 1, 0, 0, 0], dtype = numpy.uint16),
+                            numpy.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype = numpy.uint16)],
+                           [], [3, 0], ),
+                          ('case 4',
+                           [numpy.array([0, 0, 0, 0, 1, 1, 1, 0, 0, 0], dtype = numpy.uint16),
+                            numpy.array([0, 1, 0, 1, 0, 1, 0, 1, 0, 0], dtype = numpy.uint16)],
+                           [1, 1, 1], [3, 4], ),
+                          ('case 5',
+                           [numpy.array([0, 0, 0, 0, 1, 1, 1, 0, 0, 0], dtype = numpy.uint16),
+                            numpy.array([1, 0, 0, 0, 1, 1, 1, 1, 0, 0], dtype = numpy.uint16),
+                            numpy.array([0, 1, 0, 1, 0, 1, 0, 1, 0, 0], dtype = numpy.uint16)],
+                           [3, 1, 1, 1], [3, 5, 4], ),
+                          ]
+    optionsNoEdges = GenericObject()
+    optionsNoEdges.noEdges = True
     
     def buildArray(intervalList):
         a = numpy.zeros(100, dtype = numpy.uint16)
@@ -319,6 +347,16 @@ class VerifyAnalysis(unittest.TestCase):
             for a in arrayList:
                 predicted += MID.analyzeOne(a, self.options)
             self.assertEqual(predicted, gapTruth)
+            
+    def test_oneWayNoEdges(self):
+        """ analyze() should return the correct values for a given vector when NoEdges is turned on
+        """ 
+        for name, arrayList, gapTruth, coveredTruth in self.knownValuesNoEdges:
+            predicted = []
+            for a in arrayList:
+                predicted += MID.analyzeOne(a, self.optionsNoEdges)
+            self.assertEqual(predicted, gapTruth)
+
     def test_coverage(self):
         """ calcBasesCovered() should return the correct values for a given vector
         """
