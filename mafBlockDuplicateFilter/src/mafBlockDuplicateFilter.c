e86d92c5206e027d26464d4c099b8550b631eb1f
Dent Earl <dearl@soe.ucsc.edu>
2012-04-19 08:32:54 -0700

    adding ctype.h in an effort to cover circumstances when toupper() is not in stdio.h

--- mafBlockDuplicateFilter/src/mafBlockDuplicateFilter.c
+++ mafBlockDuplicateFilter/src/mafBlockDuplicateFilter.c
@@ -22,6 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE. 
  */
+#include <ctype.h>
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdint.h>
