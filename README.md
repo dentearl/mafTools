7476a5e96555ae6b28f2a53808ae1d13460c64d0
Dent Earl <dearl@soe.ucsc.edu>
2012-10-08 17:34:28 -0700

    renaming components to more accurately reflect their purpose

--- README.md
+++ README.md
@@ -20,17 +20,19 @@ With the exception of the python dependencies, when a component is missing a dep
 3. Type <code>make</code>.
 
 ## Components
-* **mafBlockDuplicateFilter** A program to filter alignment blocks to remove duplicate species. One sequence per species is allowed to remain, chosen by comparing the sequence to the consensus for the block and computing a similarity bit score between the IUPAC formatted consensus and the sequence. The highest scoring duplicate stays, or in the case of ties, the sequence closest to the start of the file stays.
-* **mafBlockExtractor** A program to extract all alignment blocks that contain a region in a particular sequence. Useful for isolating regions of interest in large maf files.
-* **mafBlockFilter** A program to filter a maf based on sequence names. Can be used to include or exclude sequence names. Useful for removing extraneous sequences from maf files.
-* **mafBlockFinder** A program to search for a position in a particular sequence. Useful for determining where in maf a particular part of the alignment resides.
-* **mafBlockSorter** A program to sort all of the blocks in a MAF based on the (absolute) start position of one of the sequences. Blocks without the sequence are placed at the start of the output in their original order.
 * **mafComparator** A program to compare two maf files by sampling. Useful when testing predicted alignments against known true alignments.
 * **mafCoveragePickles** A set of programs to assess the pairwise coverage between sequences and to extract the indel distribution of a set of sequences contained in the maf.
-* **mafStats** A set of programs to read a maf file and report back summary statistics about the file contents.
+* **mafDuplicateFilter** A program to filter alignment blocks to remove duplicate species. One sequence per species is allowed to remain, chosen by comparing the sequence to the consensus for the block and computing a similarity bit score between the IUPAC formatted consensus and the sequence. The highest scoring duplicate stays, or in the case of ties, the sequence closest to the start of the file stays.
+* **mafExtractor** A program to extract all alignment blocks that contain a region in a particular sequence. Useful for isolating regions of interest in large maf files.
+* **mafFilter** A program to filter a maf based on sequence names. Can be used to include or exclude sequence names. Useful for removing extraneous sequences from maf files.
+* **mafFinder** A program to search for a position in a particular sequence. Useful for determining where in maf a particular part of the alignment resides.
+* **mafRowOrderer** A program to order maf lines within blocks. Useful for moving a reference species to the top of all blocks. Species not specified in the ordering are automatically trimmed from the results.
+* **mafSorter** A program to sort all of the blocks in a MAF based on the (absolute) start position of one of the sequences. Blocks without the sequence are placed at the start of the output in their original order.
+* **mafStats** A program to read a maf file and report back summary statistics about the file contents.
+* **mafStrander** A program to enforce, when possible, a particular strandedness for blocks for a given species and strand orientation.
 * **mafTransitiveClosure** A program to perform the transitive closure on an alignment. That is it checks every column of the alignment and looks for situations where a position A is aligned to B in one part of a file and B is aligned to C in another part of the file. The transitive closure of this relationship would be a single column with A, B and C all present. Useful for when you have pairwise alignments and you wish to turn them into something more resembling a multiple alignment.
 * **mafValidator** A program to assess whether or not a given maf file's formatting is valid. 
 
 ## External tools
-* mafTools internal tests use Asim Jalis' [CuTest](http://cutest.sourceforge.net/) C unit testing framework. The license for CuTest is spelled out in external/license.txt.
-* mafTools internal tests will use [valgrind](http://www.valgrind.org/) if installed on your system. 
+* mafTools internal tests use Asim Jalis' [CuTest](http://cutest.sourceforge.net/) C unit testing framework (included in <code>external/</code>). The license for CuTest is spelled out in external/license.txt.
+* mafTools internal tests will use [valgrind](http://www.valgrind.org/) __if__ installed on your system. 
---
+++ mafRowOrderer/src/mafRowOrderer.c
@@ -0,0 +1,268 @@
+/* 
+ * Copyright (C) 2011-2012 by 
+ * Dent Earl (dearl@soe.ucsc.edu, dentearl@gmail.com)
+ * ... and other members of the Reconstruction Team of David Haussler's 
+ * lab (BME Dept. UCSC).
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE. 
+ */
+#include <assert.h>
+#include <errno.h> // file existence via ENOENT
+#include <getopt.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include "common.h"
+#include "sharedMaf.h"
+#include "buildVersion.h"
+
+const char *g_version = "version 0.1 October 2012";
+
+void version(void);
+void usage(void);
+void parseOptions(int argc, char **argv, char *filename, char *orderlist);
+void checkRegion(unsigned lineno, char *fullname, uint32_t pos, uint32_t start, 
+                 uint32_t length, uint32_t sourceLength, char strand);
+void printHeader(void);
+
+void version(void) {
+    fprintf(stderr, "mafRowOrderer, %s\nbuild: %s, %s, %s\n\n", g_version, g_build_date, 
+            g_build_git_branch, g_build_git_sha);
+}
+void usage(void) {
+    version();
+    fprintf(stderr, "Usage: mafBlockFilter --maf [path to maf] --order [comma separated list of species] "
+            "[options]\n\n"
+            "mafRowOrderer is a program that will look through a maf file block by block and order\n"
+            "the maf lines within a block according to the order provided. Species not in the\n"
+            "established ordered are excised. Comments are excised. Non sequnece lines ('^s') are excised.\n"
+            );
+    fprintf(stderr, "Options: \n");
+    usageMessage('h', "help", "show this help message and exit.");
+    usageMessage('m', "maf", "path to maf file.");
+    usageMessage('\0', "order", "comma separated list of sequence names.");
+    usageMessage('v', "verbose", "turns on verbose output.");
+    exit(EXIT_FAILURE);
+}
+void parseOptions(int argc, char **argv, char *filename, char *orderlist) {
+    extern int g_debug_flag;
+    extern int g_verbose_flag;
+    int c;
+    bool setMafName = false, setOrder = false;
+    while (1) {
+        static struct option longOptions[] = {
+            {"debug", no_argument, &g_debug_flag, 1},
+            {"verbose", no_argument, 0, 'v'},
+            {"help", no_argument, 0, 'h'},
+            {"version", no_argument, 0, 0},
+            {"maf",  required_argument, 0, 'm'},
+            {"order",  required_argument, 0, 0},
+            {0, 0, 0, 0}
+        };
+        int longIndex = 0;
+        c = getopt_long(argc, argv, "m:i:e:g:l:v:h",
+                        longOptions, &longIndex);
+        if (c == -1) {
+            break;
+        }
+        switch (c) {
+        case 0:
+            if (strcmp("version", longOptions[longIndex].name) == 0) {
+                version();
+                exit(EXIT_SUCCESS);
+            }
+            if (strcmp("order", longOptions[longIndex].name) == 0) {
+                setOrder = true;
+                sscanf(optarg, "%s", orderlist);
+                break;
+            }
+            break;
+        case 'm':
+            setMafName = true;
+            sscanf(optarg, "%s", filename);
+            break;
+        case 'v':
+            g_verbose_flag++;
+            break;
+        case 'h':
+        case '?':
+            usage();
+            break;
+        default:
+            abort();
+        }
+    }
+    if (!setMafName) {
+        fprintf(stderr, "specify --maf\n");
+        usage();
+    }
+    if (!setOrder) {
+        fprintf(stderr, "specify --order\n");
+        usage();
+    }
+    // Check there's nothing left over on the command line 
+    if (optind < argc) {
+        char errorString[30] = "Unexpected arguments:";
+        while (optind < argc) {
+            strcat(errorString, " ");
+            strcat(errorString, argv[optind++]);
+        }
+        fprintf(stderr, "%s\n", errorString);
+        usage();
+    }
+}
+void printHeader(void) {
+    printf("##maf version=1\n\n");
+}
+void checkBlock(mafBlock_t *mb, char **order, unsigned n) {
+    // the plan:
+    // create an array of mafLine_t linked lists, of length n
+    // walk the block, *copying* mafLines into the linked list at the coresponding array element
+    // then attach all existing array elements head to tail to form a single, ordered, 
+    // linked list. Report this. Free everything.
+    mafLine_t **lineArrayHeads = (mafLine_t**) de_malloc(sizeof(mafLine_t*) * n);
+    mafLine_t **lineArrayTails = (mafLine_t**) de_malloc(sizeof(mafLine_t*) * n);
+    unsigned i;
+    for (i = 0; i < n; ++i) {
+        lineArrayHeads[i] = NULL;
+        lineArrayTails[i] = NULL;
+    }
+    mafLine_t *ml = maf_mafBlock_getHeadLine(mb);
+    // build array
+    while (ml != NULL) {
+        if (maf_mafLine_getType(ml) != 's') {
+            ml = maf_mafLine_getNext(ml);
+            continue;
+        }
+        for (i = 0; i < n; ++i) {
+            if (strncmp(order[i], maf_mafLine_getSpecies(ml), strlen(order[i])) == 0) {
+                if (lineArrayHeads[i] == NULL) {
+                    lineArrayHeads[i] = maf_copyMafLine(ml);
+                    lineArrayTails[i] = lineArrayHeads[i];
+                } else {
+                    maf_mafLine_setNext(lineArrayTails[i], maf_copyMafLine(ml));
+                    lineArrayTails[i] = maf_mafLine_getNext(lineArrayTails[i]);
+                }
+                break;
+            }
+        }
+        ml = maf_mafLine_getNext(ml);
+    }
+    // connect array heads / tails
+    mafLine_t *head = NULL;
+    int prev = -1;
+    bool reportBlock = false;
+    for (i = 0; i < n; ++i) {
+        if (lineArrayHeads[i] == NULL) {
+            continue;
+        }
+        reportBlock = true;
+        if (head == NULL) {
+            head = lineArrayHeads[i];
+        }
+        if (prev > -1) {
+            maf_mafLine_setNext(lineArrayTails[prev], lineArrayHeads[i]);
+        }
+        prev = i;
+    }
+    // put into a dummy block
+    mafBlock_t *orderedBlock = maf_newMafBlock();
+    maf_mafBlock_setHeadLine(orderedBlock, maf_newMafLineFromString("a ordered=true", 0));
+    maf_mafLine_setNext(maf_mafBlock_getHeadLine(orderedBlock), head);
+    // report block
+    if (reportBlock) {
+        maf_mafBlock_print(orderedBlock);
+    }
+    maf_destroyMafBlockList(orderedBlock);
+    free(lineArrayHeads);
+    free(lineArrayTails);
+}
+void orderInput(mafFileApi_t *mfa, char **order, unsigned n) {
+    mafBlock_t *thisBlock = NULL;
+    bool headBlock = true;
+    printHeader();
+    while ((thisBlock = maf_readBlock(mfa)) != NULL) {
+        if (headBlock) {
+            headBlock = false;
+            maf_destroyMafBlockList(thisBlock);
+            continue;
+        }
+        checkBlock(thisBlock, order, n);
+        maf_destroyMafBlockList(thisBlock);
+    }
+}
+unsigned countNames(char *s) {
+    // counts the number of ',' characters in a string plus one
+    unsigned i, n, m;
+    m = strlen(s);
+    if (m == 0) {
+        return 0;
+    }
+    n = 1;
+    for (i = 0; i < m; ++i) {
+        if (s[i] == ',') {
+            ++n;
+        }
+    }
+    return n;
+}
+char** extractNames(char *nameList, unsigned n) {
+    // n is the number of names in the name list
+    // create a char array to hold each element in the *namelist comma separated list
+    if (n == 0) {
+        return NULL;
+    }
+    char **mat = (char**) de_malloc(sizeof(char*) * n);
+    unsigned index = 0;
+    char *tkn = NULL;
+    char *copy = de_strdup(nameList);
+    tkn = strtok(copy, ",");
+    while (tkn != NULL) {
+        mat[index] = (char*) de_malloc(sizeof(char) * (strlen(tkn) + 1));
+        strcpy(mat[index++], tkn);
+        tkn = strtok(NULL, ",");
+    }
+    free(copy);
+    return mat;
+}
+void destroyNameList(char **names, unsigned n) {
+    for (unsigned i = 0; i < n; ++i) {
+        free(names[i]);
+    }
+    free(names);
+}
+int main(int argc, char **argv) {
+    char filename[kMaxStringLength];
+    char orderlist[kMaxStringLength];
+    orderlist[0] = '\0';
+    parseOptions(argc, argv,  filename, orderlist);
+    unsigned n = countNames(orderlist);
+    char **order = extractNames(orderlist, n);
+    mafFileApi_t *mfa = maf_newMfa(filename, "r");
+    orderInput(mfa, order, n);
+    maf_destroyMfa(mfa);
+    destroyNameList(order, n);
+    return EXIT_SUCCESS;
+}
